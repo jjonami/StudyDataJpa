5258f552d0e0eb17a64ef7bc7481e4c1cf8e5cc4
yejo <yejo@upchain.kr>
2021-06-10 14:14:25 +0900

    query method

--- src/main/java/com/jjonami/datajpa/repository/MemberRepository.java
+++ src/main/java/com/jjonami/datajpa/repository/MemberRepository.java
@@ -1,10 +1,96 @@
 package com.jjonami.datajpa.repository;
 
+import com.jjonami.datajpa.dto.MemberDto;
 import com.jjonami.datajpa.entity.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    /**
+     * Data JPA > method 이름으로 query 생성
+     * => 간단한 query 작성시 사용
+     * @param username
+     * @param age
+     * @return
+     */
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    /**
+     * @Query
+     * => repository method에 query 정의
+     * => 일종의 이름 없는 Named Query
+     * @param username
+     * @param age
+     * @return
+     */
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUserByUsernameAndAge(@Param("username") String username, @Param("age") int age);
+
+    /**
+     * DTO 조회
+     * @return
+     */
+    @Query("select new com.jjonami.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
+
+    /**
+     * 컬렉션 조회
+     * @param username
+     * @return
+     */
+    List<Member> findMemberListByUsername(String username);
+
+    /**
+     * 단건 조회
+     * @param username
+     * @return
+     */
+    Member findMemberByUsername(String username);
+
+    /**
+     * Optional 단건
+     * @param username
+     * @return
+     */
+    Optional<Member> findMemberOrEmptyByUsername(String username);
+
+    /**
+     * Data JPA paging
+     * @param age
+     * @param pageable
+     * @return
+     */
+    Page<Member> findByAge(int age, Pageable pageable);
+
+    /**
+     * bulk update
+     * @param age
+     * @return
+     * bulk 연산을 하면 [PersistenceContext]에는 반영이 안된다.
+     * => bulk 연산 이후에 데이터 조회가 필요하면 [PersistenceContext]를 초기화 해줘야한다.
+     * => clearAutomatically = true
+     */
+    @Modifying(clearAutomatically = true)
+    @Query("update Member m set m.age = m.age +1 where m.age >= :age")
+    int bulkAgePlus(@Param("age") int age);
+
+    /**
+     * fetch join
+     * => N+1 문제를 해결하기 위해
+     * => [member]를 조회 할 때 query 한번으로 [team]까지 가져온다.
+     * @return
+     */
+    @Query("select m from Member m left join fetch m.team")
+    List<Member> findMemberFetchJoin();
 
 }
---
+++ src/test/java/com/jjonami/datajpa/repository/RepositoryTest.java
@@ -0,0 +1,90 @@
+package com.jjonami.datajpa.repository;
+
+import com.jjonami.datajpa.dto.MemberDto;
+import com.jjonami.datajpa.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class RepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Test
+    public void testMember(){
+        Member member = new Member("memberA");
+        Member savedMember = memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(savedMember.getId()).get();
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+
+    }
+
+    @Test
+    public void testPaging(){
+        //given
+        memberRepository.save(new Member("member1", 10));
+        memberRepository.save(new Member("member2", 10));
+        memberRepository.save(new Member("member3", 10));
+        memberRepository.save(new Member("member4", 10));
+        memberRepository.save(new Member("member5", 10));
+
+        int age = 10;
+        PageRequest pageRequest = PageRequest.of(0, 3, Sort.by(Sort.Direction.DESC, "username"));
+
+        //when
+        Page<Member> page = memberRepository.findByAge(age, pageRequest);
+        /**
+         *  Entity -> DTO 변환
+         */
+        Page<MemberDto> memberDto = page.map(member -> new MemberDto(member.getId(), member.getUsername(), null));
+
+        //then
+        List<Member> content = page.getContent();
+
+        assertThat(content.size()).isEqualTo(3);
+        assertThat(page.getTotalElements()).isEqualTo(5);
+        assertThat(page.getNumber()).isEqualTo(0);
+        assertThat(page.getTotalPages()).isEqualTo(2);
+        assertThat(page.isFirst()).isTrue();
+        assertThat(page.hasNext()).isTrue();
+    }
+
+    @Test
+    public void bulkUpdate(){
+        //given
+        memberRepository.save(new Member("member1", 10));
+        memberRepository.save(new Member("member2", 19));
+        memberRepository.save(new Member("member3", 22));
+        memberRepository.save(new Member("member4", 23));
+        memberRepository.save(new Member("member5", 34));
+
+        //when
+        int resultCnt = memberRepository.bulkAgePlus(20);
+
+        //then
+        assertThat(resultCnt).isEqualTo(3);
+    }
+
+}

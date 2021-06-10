8594f39c1903c7412d04ad3e61b8c069d5378491
yejo <yejo@upchain.kr>
2021-06-10 10:41:50 +0900

    data jpa > repository

--- src/main/java/com/jjonami/datajpa/controller/HelloController.java
+++ src/main/java/com/jjonami/datajpa/controller/HelloController.java
@@ -1,12 +1,15 @@
 package com.jjonami.datajpa.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private static final Logger log = LogManager.getLogger(HelloController.class);
 
-    @GetMapping("/hello")
+    @GetMapping("/api/v1/hello")
     public String hello(){
         return "hello";
     }
---
+++ src/main/java/com/jjonami/datajpa/entity/Member.java
@@ -0,0 +1,40 @@
+package com.jjonami.datajpa.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
+//실무에서는 Entity Setter 사용 안 하는게 좋다
+public class Member {
+
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username) {
+        this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if(team != null){
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
